f1229b97ccffdf6ddffe31fb2470b6d35f5bfea3
Tejas <tthakare73@gmail.com>
2022-05-11 18:50:17 +0530

    last commit

--- src/gameRankCard/index.js
+++ src/gameRankCard/index.js
@@ -1,15 +1,16 @@
 // import react from 'react'
 import './style.css'
+import img1 from '../assets/left.png'
 import PlayerCard from './playerCard'
 const playerCard = (props) => {
 	return(
 		<div className="main_grid_player d-flex">
 			<div className="left_card_block">
-				left block
+				<img src={img1} className="img-fluid img_CSS" />
 			</div>
-			<div className="d-flex align-item-center justify-content-center flex-wrap right_card_block px-3 py-2">
+			<div className="d-flex align-item-center justify-content-center flex-wrap right_card_block">
 				{props.cards?.map((val, index) => {
-					return (<div className="col-4 " key={index}><PlayerCard  {...val} /></div>)
+					return (<div className="col-4 pt-2 m-0" key={index}><PlayerCard  {...val} /></div>)
 				})}
 			</div>
 		</div>
