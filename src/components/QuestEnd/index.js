73349e07a3c848bb062c541544d1b2a6476ba3d4
Tejas <tthakare73@gmail.com>
2022-06-14 23:18:30 +0530

    success, greatesuccess and failure

--- src/components/QuestEnd/index.js
+++ src/components/QuestEnd/index.js
@@ -8,10 +8,10 @@ import './style.css'
 
 const CardforP = (props) => {
 	return(
-		<div className="h-full w-full rounded-2xl bg-red overflow-y-auto relative flex items-center flex-col">
-			<div className="text-white font-extrabold text-3xl mt-7 mb-12">{props.title}</div> 
-			<div className={`absolute text-3xl font-extrabold drop-shadow-3xl top-24 ${props.color}`}>{props.legd}</div>
-			<div className={`border-4 rounded-xl flex justify-center items-center flex-col p-5 mt-3 mb-1 ${props.border} ${props.bg}`}>
+		<div className="h-full w-full rounded-2xl bg-red overflow-auto relative flex flex-col items-center justify-center">
+			<div className="text-white font-extrabold text-2xl px-2  text-center md:text-3xl md:px-0 md:mt-7 mb-3 md:mb-12">{props.title}</div> 
+			<div className={`absolute text-xl md:text-3xl font-extrabold drop-shadow-3xl top-[26%] md:top-[25%] ${props.color}`}>{props.legd}</div>
+			<div className={`border-4 rounded-xl flex justify-center items-center flex-col p-3 md:p-5 mt-3 mb-1 ${props.border} ${props.bg}`}>
 			   {props.para.map((val, index) => {
 			   		return (
 			   			<p className="p-1 font-semibold" key={index}>{val}</p> 
@@ -19,7 +19,7 @@ const CardforP = (props) => {
 			   })}
 			   
  			</div>
- 			<img src={props.img} className='w-32' />
+ 			<img src={props.img} className='w-28' />
  			<div className={`font-extrabold text-transparent text-3xl bg-clip-text bg-gradient-to-t from-orange-600 to-orange-200 uppercase ${props.legd == "FAILURE" ? "hidden" : ""}`}>{props.last_text}</div>
  			<div className={`bg-gradient-to-b from-orange-600 to-yellow-300 hover:from-pink-500 hover:to-yellow-500 rounded-xl w-40 text-center font-extrabold text-white py-1 mt-1 outlines-title text-3xl ${props.legd == "FAILURE" ? "" : "hidden"}`}><div>OK</div></div>
 		</div>
@@ -27,13 +27,3 @@ const CardforP = (props) => {
 }
 
 export default CardforP;
-
-// Goblin  Attack  On The  South  Village
-
-// SUCCESS
-
-//The goblins have been repelled! 
-//Everything is right with the world!
-//Thank you so much!
-
-// Claim  Rewards
--- src/pages/QuestFailure.js
+++ src/pages/QuestFailure.js
@@ -16,8 +16,8 @@ function App() {
   ]
   return (
     <div className="appBG flex justify-center items-center flex-col h-screen relative">
-      <img src={cross} onClick={() => navigate('/goblin_attack')} className='absolute w-12 right-[23%]  top-10 z-10 hover:opacity-80 cursor-pointer'/>
-      <div className="w-6/12 h-4/5 bg-d1 rounded-2xl relative flex justify-center">
+      <img src={cross} onClick={() => navigate('/goblin_attack')} className='absolute w-12 right-0 top-[16.5%] md:right-[23.5%] md:top-[6.5%] z-10 hover:opacity-80 cursor-pointer'/>
+      <div className="w-11/12 md:w-6/12 h-3/5 md:h-4/5 bg-d1 rounded-2xl relative flex justify-center">
         <GameRankCard className="pt-5" img={img} title="Goblin  Attack  On The  South  Village" legd="FAILURE" para={para} last_text="Claim  Rewards" color="text-d2" border="border-red-600" bg="bg-red-100" />
       </div>
     </div>
